a903d9ba44528639e6ecd57517288a95d586e7c8
pierreo <pierreo@orionhealth.com>
2017-12-14 13:24:23 +0100

    removed controller, add doclist quand recherche

--- ProjetGayo/src/recherchetextuelle/view/RechercheDocController.java
+++ ProjetGayo/src/recherchetextuelle/view/RechercheDocController.java
@@ -87,6 +87,7 @@ public class RechercheDocController {
 				gestionnaire.getDocListe().add(doc);
 				i++;
 				}
+            	docTable.setItems(gestionnaire.getDocListe());
             
 			} catch (IOException | ParseException e) {
 				e.printStackTrace();
